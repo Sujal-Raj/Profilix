64f4f5d61044abf4b52d677a7f2254216fa52a80
Sujal Raj <sujalraj2064@gmail.com>
2025-12-15 23:07:02 +0530

    Updated safe version of next and added db uri in env

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function dbConnect() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/portfilioGenerator");
+        await mongoose.connect(process.env.MONGO_URI!);
     console.log("Connected")
     } catch (error) {
         console.log(error)
